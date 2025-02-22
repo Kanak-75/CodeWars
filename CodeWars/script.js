48c8b4bea781fa62a67dbf898abd38d1aea1efcd
Kanak Sharma <kanaksharma74665@gmail.com>
2025-02-22 14:29:26 +0530

    ADD

--- CodeWars/script.js
+++ CodeWars/script.js
@@ -1,5 +1,7 @@
 const moodButtons = document.querySelectorAll(".mood-btn");
 const musicFrame = document.getElementById("music-frame");
+
+// Mood-to-Music Mapping with Multiple Songs for Some Moods
 const moodMusic = {
     Happy: ["https://www.youtube.com/embed/BWczaSneA0Q"],
     
@@ -11,15 +13,7 @@ const moodMusic = {
     ],
     
     Energetic: ["https://www.youtube.com/embed/ZZ5LpwO-An4"],
-
-    Calm: [
-        "https://www.youtube.com/embed/lFcSrYw-ARY",
-        "https://www.youtube.com/embed/2OEL4P1Rz04", 
-        "https://www.youtube.com/embed/Nt8ZmiP9bzE",  
-        "https://www.youtube.com/embed/DWcJFNfaw9c",  
-        "https://www.youtube.com/embed/jgpJVI3tDbY"  
-    ],
-
+    Calm: ["https://www.youtube.com/embed/lFcSrYw-ARY"],
     Romantic: ["https://www.youtube.com/embed/BWczaSneA0Q"]
 };
 
@@ -27,13 +21,14 @@ moodButtons.forEach(button => {
     button.addEventListener("click", function () {
         const mood = this.dataset.mood;
         if (moodMusic[mood]) {
+            // Select a random song if there are multiple options
             const songs = moodMusic[mood];
             const randomSong = songs[Math.floor(Math.random() * songs.length)];
             
             musicFrame.src = randomSong + "?autoplay=1";
-            musicFrame.style.display = "block"; 
+            musicFrame.style.display = "block"; // Show the player
         } else {
-            musicFrame.style.display = "none"; 
+            musicFrame.style.display = "none"; // Hide player if no URL
         }
     });
 });
